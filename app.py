5ee0929f6aefdb27fec12538193d1ee26de84287
GEUMDOHYEON <dohyeon0518@gmail.com>
2024-01-11 07:11:11 +0900

    feat:카카오톡 오픈채팅 보내기

--- app.py
+++ app.py
@@ -188,12 +188,13 @@ def buy_join():
 
    print(item_id_receive)
 #    id_receive = request.form['id_give']
+   print(db.item.find({'user_id':item_id_receive}))
    item = db.item.find_one({'item_id':item_id_receive})
    print(item)
    print("추가 전 ", item['current_people'])
    db.item.update_one({}, {'$set': {'current_people': 0}}) # current_people 값 초기화
-   db.item.update_one({'item_id': item_id_receive}, {'$push': {'user_id':user_id_receive}})
    if item['current_people'] == item['max_people'] - 1:
+      db.item.update_one({'item_id': item_id_receive}, {'$push': {'user_id':user_id_receive}})
       people_up = item['current_people'] + 1
       print("!@#")
       print(people_up)
@@ -213,6 +214,7 @@ def buy_join():
       else:
          return jsonify({'result':'failure'})
    elif item['current_people'] < item['max_people']:
+      db.item.update_one({'item_id': item_id_receive}, {'$push': {'user_id':user_id_receive}})
       people_up = item['current_people'] + 1
       result = db.item.update_one({'item_id':item_id_receive}, {'$set': {'current_people': people_up}})
       # people_list.append() # 사용자의 아이디 받아와서 리스트에 추가하기
@@ -232,23 +234,29 @@ def share_join():
    print(item_id_receive)
 #    id_receive = request.form['id_give']
    item = db.item.find_one({'item_id':item_id_receive})
-   print(item)
-   print("추가 전 ", item['current_people'])
    db.item.update_one({}, {'$set': {'current_people': 0}}) # current_people 값 초기화
-   db.item.update_one({'item_id': item_id_receive}, {'$push': {'user_id':user_id_receive}})
-   if item['current_people'] == item['max_people'] - 1:
-      people_up = item['current_people'] + 1
-      print(people_up)
-      print("!!!@#")
-      result = db.item.update_one({}, {'$set': {'current_people': people_up}})
-      # people_list.append() # 사용자의 아이디 받아와서 리스트에 추가하기
-      print("추가 후 ",people_up)
-      print("GMAIL 전송하기")
-      msg = MIMEText(item['link'])   # 메일의 내용
-      msg['Subject'] = '공동구매를 위한 카카오톡 오픈 채팅 링크입니다.' # 메일의 제목
-      receiver = ['arevolvera@gmail.com','gold6219@naver.com','moorow0729@hufs.ac.kr'] # 전송할 메일 주소의 리스트
+   user_list = db.item.find_one({'user_id':user_id_receive,'item_id':item_id_receive},{'_id':0})
    
-    #   smtp.sendmail('dohyeon0518@gmail.com',receiver,msg.as_string())
+   kakao_link = db.item.find_one({'user_id':user_id_receive},{'_id':0})['link']
+   print(kakao_link)
+   mail_list = [db.user.find_one({'user_id':user_id_receive})['gmail']]
+   for i in user_list['user_id']:
+       mail = db.user.find_one({'user_id':i})
+       mail_list.append(mail['gmail'])
+   print(mail_list)
+   if item['current_people'] == item['max_people'] - 1:
+      db.item.update_one({'item_id': item_id_receive}, {'$push': {'user_id':user_id_receive}})
+      people_up = item['current_people'] + 1
+
+      result = db.item.update_one({'item_id': item_id_receive}, {'$set': {'current_people': people_up}})
+      # people_list.append() # 사용자의 아이디 받아와서 리스트에 추가하기
+
+      msg = MIMEText(kakao_link)   # 메일의 내용
+      msg['Subject'] = '공동구매를 위한 카카오톡 오픈 채팅 링크입니다.' # 메일의 제목
+    #   receiver = ['arevolvera@gmail.com','gold6219@naver.com','moorow0729@hufs.ac.kr'] # 전송할 메일 주소의 리스트
+      receiver = mail_list
+      print(receiver)
+      smtp.sendmail('dohyeon0518@gmail.com',receiver,msg.as_string())
       smtp.quit()
 
       if result.modified_count == 1:
@@ -256,6 +264,7 @@ def share_join():
       else:
          return jsonify({'result':'failure'})
    elif item['current_people'] < item['max_people']:
+      db.item.update_one({'item_id': item_id_receive}, {'$push': {'user_id':user_id_receive}})
       people_up = item['current_people'] + 1
       result = db.item.update_one({'item_id':item_id_receive}, {'$set': {'current_people': people_up}})
       # people_list.append() # 사용자의 아이디 받아와서 리스트에 추가하기
